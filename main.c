d527f8ba1a6d7403ee579ab86d6cf381fca5a602
Clinio Marcelino <116102989+ClinioMarcelino@users.noreply.github.com>
2024-07-15 03:21:47 -0300

    add Union and function pointer

--- main.c
+++ main.c
@@ -4,27 +4,48 @@
 #define RESET       "\x1b[0m"
 
 /*
-    Estruturas
+============================================================
+    ESTRUTURAS
+============================================================
 */
-typedef struct Mes{
-    unsigned short nContratosAssinados;
-    unsigned short horasTrabalhadas;
-}Mes;
+typedef struct CLt{
+    float salario;
+}Clt;
 
-typedef struct Funcionario{
+typedef struct Terceirizado{
+    float salarioPorHora;
+    unsigned short nHorastrabalhadas;
+}Terceirizado;
+
+// Union para utliziar a estrutura Clt ou a Terceirizada
+typedef union {
+    Clt clt;
+    Terceirizado terceirizado;
+}TipoFuncionario;
+
+typedef struct{
     char nome[30];
     _Bool isCLT; // se true/1 o funcionario é CLT, caso false/0 é Terceirizado
-    float salarioBase;
+    TipoFuncionario tipoFuncionario;
     float comissaoPorContrato;
-    Mes mes; // Possibilidade de implementacao de array de Mes para sistema com arquivo para acompanhamento dos meses
+    unsigned short nContratosAssinados;
 }Funcionario;
 
 /*
-    Prototipos das funcoes
+============================================================
+    PROTOTIPOS DAS FUNCOES
+============================================================
 */
+float (*CalcularPgmt)(Funcionario *funcionario);
 void CadastrarInformacoesFuncionario(Funcionario *funcionario);
-float CalcularPagamentoFuncionario(Funcionario funcionario);
-float CalcularFolhadePagamento(Funcionario funcionarios[],unsigned short nFuncionarios);
+float CalcularPagamentoFuncionario(Funcionario *funcionario);
+float CalcularFolhadePagamento(Funcionario funcionarios[],unsigned short nFuncionarios, float (*CalcularPgmt)(Funcionario*));
+
+/*
+============================================================
+    MAIN
+============================================================
+*/
 
 int main(){
     unsigned short nFuncionarios = 1;
@@ -32,24 +53,31 @@ int main(){
     scanf("%hu",&nFuncionarios);
 
     Funcionario funcionarios[nFuncionarios];
+
+    CalcularPgmt = CalcularPagamentoFuncionario;
     
     unsigned short contador = 0;
+    // Cadastro dos funcionarios
     do{
         CadastrarInformacoesFuncionario(&funcionarios[contador]);
     }while(++contador<nFuncionarios);
 
+    // Calculo e impressao dos totais individuais
     for(int i=0;i<nFuncionarios;i++){
         printf("Nome: %s.",funcionarios[i].nome);
-        printf("\tTotal a receber = R$ %.2f\n", CalcularPagamentoFuncionario(funcionarios[i]));
+        printf("\tTotal a receber = R$ %.2f\n", CalcularPgmt(&funcionarios[i]));
     }
 
-    printf("Total folha: R$ %.2f\n",CalcularFolhadePagamento(funcionarios,nFuncionarios));
+    // Calculo de impressao do total da folha de pagamento
+    printf("Total folha: R$ %.2f\n",CalcularFolhadePagamento(funcionarios, nFuncionarios, CalcularPgmt));
 
 return 0;
 }
 
 /*
-    Implementacao das Funcoes
+============================================================
+    IMPLEMENTACAO DAS FUNCOES
+============================================================
 */
 
 // Cadastro de informacoes de Funcionarios, recebe o ponteiro da array de funcionarios e a posicao da array
@@ -72,44 +100,40 @@ void CadastrarInformacoesFuncionario(Funcionario *funcionario){
     }
     funcionario->isCLT = aux;
 
-
-    printf("\tQual o salario do funcionario: ");
-    scanf("%f",&funcionario->salarioBase);
+    if(funcionario->isCLT){
+        printf("\tQual o salario do funcionario: ");
+        scanf("%f",&funcionario->tipoFuncionario.clt.salario);
+    }
+    else{
+        printf("\tQual o salario por hora do funcionario: ");
+        scanf("%f",&funcionario->tipoFuncionario.terceirizado.salarioPorHora);
+        printf("\tQuantas horas o funcionario trabalhou: ");
+        scanf("%hu",&funcionario->tipoFuncionario.terceirizado.nHorastrabalhadas);
+    }
 
     printf("\tQual o valor da comissao por contrato do funcionario: ");
     scanf("%f",&funcionario->comissaoPorContrato);
 
     printf("\tQuantos contratos o funcionario assinou no mes: ");
-    scanf("%hu",&funcionario->mes.nContratosAssinados);
-
-    // Caso o funcionario nao seja CLT pergnta e cadastra o numero de horas trabalhadas no mes
-    if (!funcionario->isCLT){
-        printf("\tQuantas horas o funcionario trabalhou: ");
-        scanf("%hu",&funcionario->mes.horasTrabalhadas);
-
-    }
-    // Se o funcioanrio for CLT o "numero de horas trabalhadas": 1. Nao interfere na base de calculo do salario mensal  
-    else
-        funcionario->mes.horasTrabalhadas = 1;
+    scanf("%hu",&funcionario->nContratosAssinados);
 
     printf("\n");
 
 }
 
 // Calcula e retorna o Total a receber do funcionario seja CLT ou Terceirizado. 
-// Comissao por contrato e nContratos assinados funciona igual para ambos.
-// SalarioBase do CLT: valor total mensal que multiplica por 1. 
-// Terceirizados tem o SalarioBase como o valor por hora e multiplica pela quantidade de horas trabalhadas no mes.
-float CalcularPagamentoFuncionario(Funcionario funcionario){
-    return(funcionario.salarioBase*funcionario.mes.horasTrabalhadas) + 
-    (funcionario.comissaoPorContrato*funcionario.mes.nContratosAssinados);
+float CalcularPagamentoFuncionario(Funcionario *funcionario){
+    // operacao ternaria para verificar se e Clt ou Tercerizado e calcular de acordo
+    float aux = funcionario->isCLT ? funcionario->tipoFuncionario.clt.salario : 
+        funcionario->tipoFuncionario.terceirizado.nHorastrabalhadas * funcionario->tipoFuncionario.terceirizado.salarioPorHora;
+    return aux + (funcionario->comissaoPorContrato*funcionario->nContratosAssinados);
 }
 
-// Variavel totalFolha vai somando o pagamento de cada funcionario utilizando CalcularPagamentoFuncionario() e retorna o total da folha.
-float CalcularFolhadePagamento(Funcionario funcionarios[],unsigned short nFuncionarios){
+// Variavel totalFolha vai somando o pagamento de cada funcionario utilizando o function pointer de Calcular Pgmt e retorna o total da folha.
+float CalcularFolhadePagamento(Funcionario funcionarios[], unsigned short nFuncionarios, float (*CalcularPgmt)(Funcionario*)){
     float totalFolha = 0;
     for(int i=0;i<nFuncionarios;i++){
-        totalFolha += CalcularPagamentoFuncionario(funcionarios[i]);
+        totalFolha += CalcularPgmt(&funcionarios[i]);
     }
 
     return totalFolha;
